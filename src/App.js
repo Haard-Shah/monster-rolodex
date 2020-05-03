dba23eee6181128b193d477fe18d241dba41482d
Haard <haard2014@outlook.vom>
2020-05-03 18:54:21 +1000

    Initial commit, adding all the pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import "./App.css";
+import { SearchBox } from "./components/search-box/search-box.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super(); // call parent constructor
+
+    this.state = {
+      monsters: [],
+      searchFiled: "",
+    };
+  }
+
+  //USEFUL: for assessment API calls
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((Response) => Response.json())
+      // .then((users) => console.log(users));
+      .then((users) => this.setState({ monsters: users }))
+      .catch((error) => console.log(error));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchFiled: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchFiled } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchFiled.toLowerCase())
+    ); //this will refilter the array every time render() is called which in this case would be onchange() for input
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="Search Monsters"
+          handleChange={this.handleChange} // parsing a functions as a call for onchange()
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
