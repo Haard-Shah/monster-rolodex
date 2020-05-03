04c4b6f7bf7100dba1c638bfcbaeebb07c8ed5e4
Haard <haard2014@outlook.vom>
2020-05-03 18:41:42 +1000

    Updates

---
+++ precache-manifest.4510159028982a502cf6092eab5a5654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31343cf98df0cad0d6f46945f3229b5e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bac53cb31d51ca4b4350",
+    "url": "/monster-rolodex/static/css/main.2237ab2a.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monster-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac53cb31d51ca4b4350",
+    "url": "/monster-rolodex/static/js/main.87c215d4.chunk.js"
+  },
+  {
+    "revision": "e4eba3394aec38fdb39c",
+    "url": "/monster-rolodex/static/js/runtime-main.2047114a.js"
+  }
+]);
